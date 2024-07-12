1c2f43f025241d4f5bcd9a0f93b3aceaf25b20f4
Douglas Moreira <dougmoreira@Douglass-MBP.lan>
2024-07-12 15:39:14 -0400

    HomePage Layout/ Form setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,17 @@
 import PatientForm from "@/components/PatientForm";
-import { Button } from "@/components/ui/button";
+//import { PasskeyModal } from "@/components/PasskeyModal";
 import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
+  //const isAdmin = searchParams?.admin === "true";
   return (
     <div className="flex h-screen max-h-screen">
-      {/* TODO: OTP VERIFICATION \ PASSKEYMODAL */} 
+      {/* {isAdmin && <PasskeyModal />} */}
+      {/* TODO: OTP VERIFICATION \ PASSKEYMODAL */}
       <section className="remove-scrollbar container my-auto">
-        <div className="sub-container mac-w-[496px]">
-          <Image 
+        <div className="sub-container max-w-[496px]">
+          <Image
             src="/assets/icons/logo-full.svg"
             height={1000}
             width={1000}
@@ -17,14 +19,18 @@ export default function Home() {
             className="mb-12 h-10 w-fit"
           />
           <PatientForm />
-          <div className="text-14-regular mt-20 flex justify-between ">
-          <p className="justify-items-end text-dark-600 xl:text-left">© 2024 CareConnect</p>
-          <Link href="/?admin=true" className="text-green-500">Admin</Link>
+          <div className="text-14-regular mt-20 flex justify-between">
+            <p className="justify-items-end text-dark-600 xl:text-left">
+              © 2024 CareConnect
+            </p>
+            <Link href="/?admin=true" className="text-green-500">
+              Admin
+            </Link>
           </div>
         </div>
       </section>
-      <Image 
-        src='/assets/images/onboarding-img.png'
+      <Image
+        src="/assets/images/onboarding-img.png"
         height={1000}
         width={1000}
         alt="patient"
--- components/PatientForm.tsx
+++ components/PatientForm.tsx
@@ -1,9 +1,104 @@
-import React from 'react'
+"use client";
 
-const PatientForms = () => {
-  return (
-    <div>PatientForm</div>
-  )
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { useRouter } from "next/navigation";
+import {
+  Form
+} from "@/components/ui/form";
+import CustomFormField from "./CustomFormField";
+import SubmitButton from "./SubmitButton";
+import { useState } from "react";
+import { UserFormValidation } from "@/lib/validation";
+export enum FormFieldType {
+  INPUT = 'input',
+  TEXTAREA = 'textarea',
+  PHONE_INPUT = 'phoneInput',
+  CHECKBOX = 'checkbox',
+  DATE_PICKER = 'datePicker',
+  SELECT = 'select',
+  SKELETON = 'skeleton'
 }
 
-export default PatientForms
+const formSchema = z.object({
+  username: z.string().min(2, {
+    message: "Username must be at least 2 characters.",
+  }),
+});
+
+const PatientForm = () => {
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
+
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof UserFormValidation>>({
+    resolver: zodResolver(UserFormValidation),
+    defaultValues: {
+      name: "",
+      email: "",
+      phone: "",
+    },
+  });
+
+  // 2. Define a submit handler.
+  const onSubmit = async (values: z.infer<typeof UserFormValidation>) => {
+    setIsLoading(true);
+
+    try {
+      // const user = {
+      //   name: values.name,
+      //   email: values.email,
+      //   phone: values.phone,
+      // };
+
+      // const newUser = await createUser(user);
+
+      // if (newUser) {
+      //   router.push(`/patients/${newUser.$id}/register`);
+      // }
+    } catch (error) {
+      console.log(error);
+    }
+
+    setIsLoading(false);
+  };
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="flex-1 space-y-6">
+        <section className="mb-12 space-y-4">
+          <h1 className="header">Hello</h1>
+          <p className="text-dark-700">Schedule your appointment.</p>
+        </section>
+        <CustomFormField
+          fieldType={FormFieldType.INPUT}
+          name="Name"
+          label="Full Name"
+          placeholder="Mario Pellucci"
+          iconSrc="/assets/icons/user.svg"
+          iconAlt="user"
+          control={form.control}
+        />
+        <CustomFormField
+          fieldType={FormFieldType.INPUT}
+          name="email"
+          label="Email"
+          placeholder="mariopellucci@makewondr.co"
+          iconSrc="/assets/icons/email.svg"
+          iconAlt="email"
+          control={form.control}
+        />
+        <CustomFormField
+          fieldType={FormFieldType.PHONE_INPUT}
+          name="phone"
+          label="Phone Number"
+          placeholder="(555)123-4567"
+          control={form.control}
+        />
+        <SubmitButton  isLoading={isLoading}>Get Started</SubmitButton>
+      </form>
+    </Form>
+  );
+};
+
+export default PatientForm;
